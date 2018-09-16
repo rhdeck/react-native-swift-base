40d8b5365a016af93bf9d3dfe48c4b6c00918d9b
Ray Deck <ray@raydeck.com>
2018-09-16 09:15:04 -0400

    Adding startup helpers to make sure to register events from SLs

--- templates/AppDelegate.swift
+++ templates/AppDelegate.swift
@@ -2,6 +2,24 @@ import UIKit
 import RNSRegistry
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+  var ranAtStart = false
+  public func runAtStart() {
+    //No guaranteed thread
+    guard !ranAtStart else { return }
+    ranAtStart = true
+    let namespace = (Bundle.main.infoDictionary!["CFBundleExecutable"] as! String).replacingOccurrences(of: "-", with: "_").replacingOccurrences(of: " ", with: "_")
+    guard let classes = Bundle.main.infoDictionary!["RNSRClasses"] as? [String] else { return }
+    classes.forEach() { c in
+      if let cl  = NSClassFromString(c) {
+        cl.runOnStart?()
+      } else {
+        let fqn =  namespace + "." + c
+        if let cl = NSClassFromString(fqn) {
+          cl.runOnStart?()
+        }
+      }
+    }
+  }
   var window: UIWindow?
   var cachedLaunchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil
   public func getRootView(_ jsLocation: URL) -> UIView? {
@@ -12,20 +30,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
   //MARK: Lifecycle Management
   public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    runAtStart()
     cachedLaunchOptions = launchOptions
     guard let jsLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackResource: nil) else {
       return false;
     }
     RNSMainRegistry.setData(key: "initialBundle", value: jsLocation.absoluteString)
-    let _ = RNSMainRegistry.triggerEvent(type: "app.didFinishLaunchingWithOptions.start", data: [:])
+    RNSMainRegistry.triggerEvent(type: "app.didFinishLaunchingWithOptions.start", data: [:])
     let w = UIWindow(frame: UIScreen.main.bounds)
     let rvc = UIViewController()
-    if let s = RNSMainRegistry.getData(key: "initialBundle") as? String, let u = URL(string: s) {
-      rvc.view = getRootView(u)
-    } else {
-      rvc.view = getRootView(jsLocation)
-    }
-    rvc.view = getRootView(sURL)
+    rvc.view = getRootView(jsLocation)
     w.rootViewController = rvc
     w.makeKeyAndVisible()
     self.window = w
@@ -63,36 +77,48 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
   //MARK:Shortcut Management
   public func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
-      RNSMainRegistry.setData(key: "shortcuttriggered", value: shortcutItem.type)
-      RNSMainRegistry.setData(key: shortcutItem.type, value: shortcutItem.userInfo ?? [:])
+    runAtStart()
+    RNSMainRegistry.setData(key: "shortcuttriggered", value: shortcutItem.type)
+    RNSMainRegistry.setData(key: shortcutItem.type, value: shortcutItem.userInfo ?? [:])
     let ret = RNSMainRegistry.triggerEvent(type: shortcutItem.type, data: shortcutItem.userInfo ?? [:])
       completionHandler(ret)
   }
   //MARK:URL Management
   public func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+    runAtStart()
     RNSMainRegistry.setData(key: "app.url", value:url)
     RNSMainRegistry.setData(key: "app.urlinfo", value: options)
     let ret = RNSMainRegistry.triggerEvent(type: "app.openedurl", data: url)
     return ret
   }
-  //MARK: Notification Management
+  //MARK:Notification Management
   public func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    runAtStart()
     let _ = RNSMainRegistry.triggerEvent(type: "app.didRegisterForRemoteNotifications", data: deviceToken)
   }
   public func application(_ application: UIApplication, didRegister notificationSettings: UIUserNotificationSettings) {
+    runAtStart()
     let _ = RNSMainRegistry.triggerEvent(type: "app.didRegisterUserNotificationSettings", data: notificationSettings)
   }
   public func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any]) {
+    runAtStart()
     let _ = RNSMainRegistry.triggerEvent(type: "app.didReceiveRemoteNotification", data: userInfo)
   }
   public func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    runAtStart()
     let _ = RNSMainRegistry.triggerEvent(type: "app.didFailToRegisterForRemoteNotifications", data: error)
   }
   public func application(_ application: UIApplication, didReceive notification: UILocalNotification) {
+    runAtStart()
     let _ = RNSMainRegistry.triggerEvent(type: "app.didReceiveLocalNotification", data: notification)
   }
-  //MARK: Background download handling
   public func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
+    runAtStart()
     let _ = RNSMainRegistry.triggerEvent(type: "app.handleEventsForBackgroundURLSession", data: ["identifier": identifier, "completionHandler": completionHandler])
   }
+}
+//Helper to give the runOnStart hint for AnyClass
+@objc(startable)
+class startable:NSObject {
+  @objc class func runOnStart() {}
 }
